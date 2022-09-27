3bd34a31c3c83844e2ddc0f5c1a0fc202501f241
George <nabuage@gmail.com>
2022-09-26 22:41:59 -0500

    Change router.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom";
 import TodoContainer from "./functionBased/components/TodoContainer";
 import About from "./functionBased/pages/About";
 import "./functionBased/App.css";
-import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import NotMatch from "./functionBased/pages/NotMatch";
 import Navbar from "./functionBased/components/Navbar";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-        <Navbar />
+        <HashRouter>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<TodoContainer />} />
                 <Route path="/about/*" element={<About />} />
                 <Route path="*" element={<NotMatch />} />
             </Routes>
-        </BrowserRouter>        
+        </HashRouter>        
     </React.StrictMode>,
     document.getElementById("root"));
